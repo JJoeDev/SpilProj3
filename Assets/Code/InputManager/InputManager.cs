a8f45d8b737503dfe9205dfb6b182eacc44c729e
JJoe <48448023+JJoeDev@users.noreply.github.com>
2025-08-18 11:06:33 +0200

    Input Manager Singleton

---
+++ Assets/Code/InputManager/InputManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager _instance;
+    public static InputManager Instance { get { return _instance; } }
+
+    PlayerInputs pInput;
+
+    private void Awake() {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        pInput = new PlayerInputs();
+    }
+
+    private void OnEnable() => pInput.Enable();
+    private void OnDisable() => pInput.Disable();
+
+    public Vector2 OnMove() => pInput.Player.Move.ReadValue<Vector2>().normalized;
+    public Vector2 OnLook() => pInput.Player.Look.ReadValue<Vector2>();
+}
