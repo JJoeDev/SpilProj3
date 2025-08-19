d6e59a7cfac17995d224174c68cee70a66b8ee19
Patrick <63421519+RexStarGame@users.noreply.github.com>
2025-08-19 22:31:24 +0200

    update

--- Assets/Code/CollisionDamage/PlayerMovementTest.cs
+++ Assets/Code/CollisionDamage/PlayerMovementTest.cs
@@ -7,7 +7,7 @@ public class PlayerMovementTest : MonoBehaviour
     public float currentSpeed; // hvor hurtigt billen køre lige nu... 
     public float minSpeed; // bilens min speed. 
     public float MaxSpeed; // bilens max speed. 
-    public float speed => currentSpeed; // for at kunne læse nuværende fart fra  script = collisionDamage ! 
+    public float speed => currentSpeed; // for at kunne læse nuværende fart fra  script = collisionDamage &&  ! 
     // Start is called before the first frame update
     void Start()
     { 
