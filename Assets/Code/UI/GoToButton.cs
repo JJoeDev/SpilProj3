8f0d670280d9f32076b20ebe3606e469eba5ece9
Steamy <toretordenguden@gmail.com>
2025-08-19 10:52:22 +0200

    Go to button added

---
+++ Assets/Code/UI/GoToButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GoToButton : MenuButton
+{
+    [SerializeField] GameObject m_currentMenuObject; // The object to disable
+    [SerializeField] GameObject m_goToObject;        // The object to enable
+
+    public override void OnPointerClick(PointerEventData pointerEventData)
+    {
+        m_goToObject.SetActive(true);
+        m_currentMenuObject.SetActive(false);
+    }
+
+    public override void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        base.OnPointerEnter(pointerEventData);
+    }
+
+    public override void OnPointerExit(PointerEventData pointerEventData)
+    {
+        base.OnPointerExit(pointerEventData);
+    }
+}
