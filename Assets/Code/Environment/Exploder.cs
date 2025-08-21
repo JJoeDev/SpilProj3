005427d3d7ca88f044e1233e58f6723aa184b6e3
Steamy <toretordenguden@gmail.com>
2025-08-21 09:51:58 +0200

    Added barrel that affects IExplodable objects

---
+++ Assets/Code/Environment/Exploder.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploder : MonoBehaviour
+{
+    [SerializeField] float m_explodeRadius = 5;
+    [SerializeField] float m_explosionForce;
+
+
+    Collider[] m_affectedObjects;
+    int m_affectedObjectsCount = 20;
+
+    void Explode()
+    {
+        Debug.Log("EXPLODING!");
+
+        m_affectedObjects = new Collider[m_affectedObjectsCount];
+        Physics.OverlapSphereNonAlloc(transform.position, m_explodeRadius, m_affectedObjects);
+        
+        foreach (Collider collider in m_affectedObjects)
+        {
+            if (collider == null || collider.gameObject == gameObject) continue;
+
+            if (collider.GetComponent<IExplodable>() != null)
+            {
+                collider.GetComponent<IExplodable>().Explode();
+
+                Debug.Log(collider.name);
+                Debug.DrawLine(transform.position, collider.transform.position, Color.red, 5f);
+            }
+
+        }
+
+        //Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Explode();
+        }
+    }
+}
