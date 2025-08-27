5df013636879054d86e0d9b261e8f04d88280669
Mixemand <sandervegener@gmail.com>
2025-08-27 21:44:27 +0200

    New CollisionManager (not working properly yet)
    Both cars still take damage upon impact from the side :(

--- Assets/Code/Environment/ExplosionScript.cs
+++ Assets/Code/Environment/ExplosionScript.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehicleExplosion : MonoBehaviour {
+public class VehicleExplosion : Explodable 
+{
     [SerializeField] private GameObject m_explosion;
-   public void Explode()
+    [SerializeField] private float  health;
+   public void Explodes()
     {
         m_explosion = Instantiate(m_explosion, transform.position, Quaternion.identity);
         Instantiate(m_explosion);
         GameObject.Destroy(gameObject);
     }
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        Debug.Log(gameObject.name + " took " + amount + " damage");
+
+        if (health <= 0f)
+        {
+            Explodes();
+        }
+    }
+
 }
---
+++ Assets/Code/Upgrades/Plov.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plov : Upgrade
+{
+    [SerializeField] GameObject plov;
+
+    
+}
