f29f9319a52d5abdfc094d9e3b0c5ed15be5c076
Steamy <toretordenguden@gmail.com>
2025-08-28 23:01:10 +0200

    camrafollowa

--- Assets/Code/PreGameCameraFollow.cs
+++ Assets/Code/PreGameCameraFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PreGameCamera : MonoBehaviour
+public class PreGameCameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject[] m_cameraFollowPoints;
     [SerializeField] GameObject m_car;
