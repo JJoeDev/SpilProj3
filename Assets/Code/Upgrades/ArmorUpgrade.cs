934dd7359941c77952143feccc54a73dccd5e883
Steamy <toretordenguden@gmail.com>
2025-08-29 10:19:50 +0200

    Upgrade roadmap works - missing actual upgrades

--- Assets/Code/Upgrades/ArmorUpgrade.cs
+++ Assets/Code/Upgrades/ArmorUpgrade.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class ArmorUpgrade : Upgrade
 {
-    public float armorHealth = 50f;
+    public float armorHealth = 150f;
     public GameObject armorVisual;
 
-    bool applied;
-
-    private void Start()
-    {
-        // Enables the ability at the start of the game
-        if (!applied) { EnableUpgrade(); applied = true; }
-    }
     public override void EnableUpgrade()
     {
         // Adds health
--- Assets/Code/Upgrades/UpgradeManager.cs
+++ Assets/Code/Upgrades/UpgradeManager.cs
@@ -6,21 +6,35 @@ using UnityEngine;
 public class UpgradeManager : MonoBehaviour
 {
     [SerializeField] private UIMeter m_scoreMeter;
-    [SerializeField] private UpgradeCard[] upgradeCards;
+    [SerializeField] private UpgradeCard[] m_upgradeCards;
+    [SerializeField] private GameObject m_upgradeMenu;
+    private InputManager m_inputManager;
+
     public Upgrade[] upgrades;
 
     public int upgradeCount = 0;
 
+    private void Start()
+    {
+        m_inputManager = InputManager.Instance;
+    }
+
     private void Update()
     {
-        if(upgradeCount < upgradeCards.Length)
+        if (m_inputManager.OnOpenUpgradeRoadmap().triggered)
+        {
+            m_upgradeMenu.SetActive(!m_upgradeMenu.activeSelf);
+            Cursor.lockState = m_upgradeMenu.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+
+        if(upgradeCount < m_upgradeCards.Length)
         {
             if (m_scoreMeter.value >= (m_scoreMeter.maxValue / 3) * (upgradeCount + 1))
             {
-                Debug.Log("Got upgrade: " + upgradeCards[upgradeCount].name);
+                Debug.Log("Got upgrade: " + m_upgradeCards[upgradeCount].name);
                 m_scoreMeter.value = 0;
-                upgradeCards[upgradeCount].enabled = true;
-                upgradeCards[upgradeCount].UpdateCard();
+                m_upgradeCards[upgradeCount].enabled = true;
+                m_upgradeCards[upgradeCount].UpdateCard();
                 upgradeCount++;
             }
         }
