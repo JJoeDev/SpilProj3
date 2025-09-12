32a673e38ee3fe79279ac1c820fe9529193e4d20
Steamy <toretordenguden@gmail.com>
2025-09-12 13:43:45 +0200

    In world health bar woprks

--- Assets/Code/HealthBar.cs
+++ Assets/Code/HealthBar.cs
@@ -6,19 +6,24 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] HealthManager m_healthManager;
     [SerializeField] Transform m_bar;
+    [SerializeField] float m_offsetPos; // How much to move the bar left as the health scales
 
     Vector3 m_startScale;
+    Vector3 m_startPos;
     Camera m_mainCam;
 
     private void Start()
     {
         m_startScale = m_bar.transform.localScale;
+        m_startPos = m_bar.transform.localPosition;
         m_mainCam = Camera.main;
     }
 
     void Update()
     {
+        float m_t = (m_healthManager.currentHealth / m_healthManager.maxHealth);
         transform.LookAt(m_mainCam.transform);
-        m_bar.transform.localScale = new Vector3(m_startScale.x * (m_healthManager.currentHealth / m_healthManager.maxHealth), m_startScale.y, m_startScale.z);
+        m_bar.transform.localScale = new Vector3(m_startScale.x * m_t, m_startScale.y, m_startScale.z);
+        m_bar.localPosition = new Vector3(m_startPos.x - m_offsetPos * (1f - m_t), m_startPos.y, m_startPos.z);
     }
 }
