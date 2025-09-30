81e3237210a3647c0f83dc964f257000e139d32b
Steamy <toretordenguden@gmail.com>
2025-09-30 13:32:21 +0200

    Updated mainmenu, and added xray to enemy

--- Assets/Code/Environment/BarrelExplosion.cs
+++ Assets/Code/Environment/BarrelExplosion.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -62,3 +62,4 @@ public class BarrelExplosion : Explodable
     }
 }
 
+*/
