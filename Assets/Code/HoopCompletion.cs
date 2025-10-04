e0c5723ccf4e50557adf2ddb2a7ef9c86ecd9909
BDSM Duen <149476617+Mixemand@users.noreply.github.com>
2025-10-04 16:06:31 +0200

    Hoop detection works

---
+++ Assets/Code/HoopCompletion.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopCompletion : MonoBehaviour
+{
+    [SerializeField] private List<Collider> m_hoopsToHitOriginal;
+    [SerializeField] private GameObject m_bigHoop;
+    private List<Collider> m_hoopsToHitRuntime;
+    public bool smallHoopsCompleted = false;
+    public bool bigHoopCompleted = false;
+
+    private void Awake()
+    {
+        m_hoopsToHitRuntime = new List<Collider>(m_hoopsToHitOriginal);
+        if (!smallHoopsCompleted)
+        {
+            m_bigHoop.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_hoopsToHitRuntime.Contains(other))
+        {
+            m_hoopsToHitRuntime.Remove(other);
+            Destroy(other.gameObject);
+        }
+        if (!smallHoopsCompleted && m_hoopsToHitRuntime.Count == 0)
+        {
+            smallHoopsCompleted = true;
+            if (m_bigHoop != null) m_bigHoop.SetActive(true);
+        }
+
+        if (smallHoopsCompleted && m_bigHoop != null && other.gameObject == m_bigHoop)
+        {
+            Destroy(m_bigHoop);
+            m_bigHoop = null;
+            bigHoopCompleted = true;
+        }
+    }
+}
